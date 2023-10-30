8746efe8496b7159f0fe69be51c5f8b99d667d44
PY44N <pyan4444@gmail.com>
2023-10-30 19:12:44 -0400

    I hate all of you

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // the UI component for filtering the subway entrances by subway line
 export default (props) => {
-  // const { location, setLocation } = props;
-  const [location, setLocation] = useState('Jeff');
+  const [location, setLocation] = useState("");
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long);
+  }, [lat, long]);
+
+  function getPosition() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long);
+  }
 
   // this is the JSX that will become the Filter UI in the DOM, notice it looks pretty similar to HTML
   // notice in the select element onChange is set to the updateFilter method
@@ -13,8 +35,9 @@ export default (props) => {
     <div className="filterSubwayLines">
       <hr/>
       <h3>Location</h3>
-      <p>{location}</p>
-      <input type='text' value={location} onChange={setLocation}></input>      
+      <p>{lat}, {long}</p>
+      <input type='text' value={location} onChange={(val) => setLocation(val.currentTarget.value)}></input>
+      <button onClick={getPosition}>Get Current Position</button>     
     </div>
   );
 };
