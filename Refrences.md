fd8affa904dd3a962ae82f28716ae2a0905c9749
PY44N <pyan4444@gmail.com>
2023-10-30 17:26:53 -0400

    Map is a thing

--- Refrences.md
+++
@@ -1,3 +0,0 @@
-- https://doi.org/10.2166/wqrj.2018.025
-- https://doi.org/10.1016/j.jhydrol.2019.124084
-- https://doi.org/10.3390/w11112210
--- src/main.py
+++
@@ -1,8 +0,0 @@
-import data.NCEI as NCEI
-import data.USGS as USGS
-# import ai
-
-#data = data.USGS.getTrainingData()
-#ai.train(data)
-
-print(NCEI.getData('2023-10-1', '2023-10-3'))
